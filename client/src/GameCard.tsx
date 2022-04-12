30ab2603553b9844e1226a8a9e763a44446b02f0
Mark Sorial <msorial44@gmail.com>
2022-04-12 22:48:32 +0400

    small fix

--- client/src/GameCard.tsx
+++ client/src/GameCard.tsx
@@ -31,7 +31,7 @@ function GameCard(props: any) {
     if(refresh && props.cardPos === 2) {
         setTimeout(() => {
             setRefresh(false);
-            axios.get('/api/getGame').then((res) => {
+            axios.get('/server/api/getGame').then((res) => {
                 console.log(res.data);
                 props.refreshCallback();
                 setName(res.data.name);
