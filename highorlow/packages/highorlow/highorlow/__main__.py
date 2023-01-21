2fe576f0b9261d499be7fc6c1aeda89709438132
Mark Sorial <msorial44@gmail.com>
2023-01-21 22:07:50 +0400

    using numpy instead of pandas

--- highorlow/packages/highorlow/highorlow/__main__.py
+++ highorlow/packages/highorlow/highorlow/__main__.py
@@ -1,18 +1,18 @@
-from http import HTTPStatus
-import pandas as pd
+import random
+import numpy as np
 
 def main(args):
-    df = pd.read_csv('./games-data-final.csv')
-    row = df.sample()
+    df = np.loadtxt("/work/games-data-final.csv",delimiter=",", dtype=str, skiprows=1, quotechar='"')
+    row = random.sample(range(0, df.shape[1]-1), 1)
     game = {
-        "name": row['name'].values[0],
-        "platform": row['platform'].values[0],
-        "rdate": row['r-date'].values[0],
-        "score": row['score'].values[0],
-        "image": row['image-link'].values[0]
+        "name": df[row][0][1],
+        "platform": df[row][0][2],
+        "rdate": df[row][0][3],
+        "score": df[row][0][4],
+        "image": df[row][0][5]
     }
     return {
-        "statusCode": HTTPStatus.OK,
+        "statusCode": 200,
         "body": game
     }
     
